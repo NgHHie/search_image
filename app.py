a13e9a5b3d5b3b24cb765c9ee197b18deefbe3d8
Nguyễn Hoàng Hiệp <hiep2003ka@gmail.com>
2024-09-10 16:54:01 +0700

    heheh

---
+++ app.py
@@ -0,0 +1,146 @@
+from flask import Flask, request, jsonify, send_file, url_for
+from flask_cors import CORS
+from pymongo import MongoClient
+from io import BytesIO
+from PIL import Image
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import os
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tensorflow.keras.preprocessing import image
+from scipy.spatial.distance import cdist
+import base64
+
+# Khởi tạo mô hình ResNet50
+model2 = ResNet50(weights='imagenet', include_top=False, pooling='avg')
+
+def extract_features(img, xyxy):
+    features = []
+    for box in xyxy:
+        # Chuyển đổi tọa độ từ [x1, y1, x2, y2] thành (left, upper, right, lower)
+        x1, y1, x2, y2 = map(int, box)
+
+        # Cắt ảnh
+        cropped_img = img.crop((x1, y1, x2, y2))
+
+        # Đảm bảo kích thước của ảnh cắt ra là (224, 224)
+        cropped_img = cropped_img.resize((224, 224))
+
+        # Chuyển đổi ảnh thành tensor
+        img_array = image.img_to_array(cropped_img)
+        img_array = np.expand_dims(img_array, axis=0)
+
+        # Tiền xử lý ảnh cho mô hình ResNet50
+        img_array = preprocess_input(img_array)
+
+        # Đưa ảnh vào mô hình để lấy đặc trưng
+        features_batch = model2.predict(img_array)
+        features.append(features_batch.flatten())
+
+    return features
+
+app = Flask(__name__)
+CORS(app)
+
+model = YOLO("weight/lastv3.pt")
+
+# ---MongoDB setup---
+mongo_client = MongoClient('mongodb://localhost:27017/')
+db = mongo_client['search_image']
+collection = db['image_features']
+
+names = ['Tshirt', 'dress', 'jacket', 'pants', 'shirt', 'short', 'skirt', 'sweater']
+
+@app.route('/')
+def home():
+    return send_file('search_image.html')
+
+
+@app.route('/diabetes/v1/predict', methods=['POST'])
+def predict():
+    # ---get the image from the request---
+    image_file = request.files['image'].read()  # Lấy ảnh từ request
+    img = Image.open(BytesIO(image_file))  # Chuyển đổi ảnh thành đối tượng PIL
+
+    # ---convert PIL image to OpenCV format (numpy array)---
+    img_cv2 = np.array(img.convert('RGB'))  # Chuyển từ PIL sang numpy (OpenCV dùng RGB)
+
+    # ---process image using YOLO model---
+    results = model(img)  # Sử dụng YOLO model để xử lý ảnh
+    img_features = extract_features(img, results[0].boxes.xyxy)
+    cls_array = results[0].boxes.cls.numpy()
+    tags = [names[int(idx)] for idx in cls_array]
+    conditions = [{'tags': tag} for tag in tags]
+    query = {'$or': conditions}
+    db_imgs = list(collection.find(query))
+    db_features_list = []
+    for db_img in db_imgs:
+        features = db_img.get('features', [])
+        db_img_tags = db_img.get('tags', [])
+        # Lặp qua các feature trong 'features'
+        for idx, feature in enumerate(features):
+            if db_img_tags[idx] in tags:
+                db_features_list.append(feature)
+    img_features_array = np.array(img_features)
+    db_features_array = np.array(db_features_list)
+    distance_matrix = cdist(img_features_array, db_features_array, metric='euclidean')
+
+    cnt = 0
+    keyed_dict = {}
+    for db_img in db_imgs:
+        # Truy cập mảng 'features' thông qua key 'features'
+        features = db_img.get('features', [])
+        db_img_tags = db_img.get('tags', [])
+        # Lặp qua các feature trong 'features'
+        for idx, feature in enumerate(features):
+            if db_img_tags[idx] in tags:
+                keyed_dict[cnt] = db_img['filename']  # Truy cập thông qua key 'filename'
+                cnt += 1
+
+    sorted_indices = np.argsort(distance_matrix, axis=1)
+    sorted_distances = np.take_along_axis(distance_matrix, sorted_indices, axis=1)
+    list_product = []
+    for column in sorted_indices:
+        for element in column:
+            if keyed_dict[element] not in list_product:
+                list_product.append(keyed_dict[element])
+    print(list_product)
+
+    for db_img in db_imgs:
+        print(db_img['filename'])
+    for result in results:
+        for box in result.boxes.xyxy:  # Lấy tọa độ xyxy của các bounding box
+            x1, y1, x2, y2 = map(int, box)  # Chuyển tọa độ sang integer
+            # Vẽ bounding box lên ảnh
+            cv2.rectangle(img_cv2, (x1, y1), (x2, y2), (0, 255, 0), 2)  # Vẽ ô vuông màu xanh lá (0, 255, 0)
+
+    # ---convert the processed image (with bounding boxes) back to PIL---
+    processed_image_pil = Image.fromarray(img_cv2)  # Chuyển lại sang PIL để trả về
+
+    # ---save the processed image to a byte array---
+    img_byte_arr = BytesIO()
+    processed_image_pil.save(img_byte_arr, format='PNG')  # Lưu ảnh đã xử lý vào byte array
+    img_byte_arr.seek(0)
+    img_base64 = base64.b64encode(img_byte_arr.getvalue()).decode('utf-8')
+
+    # ---send the processed image as a response---
+    image_urls = [url_for('get_image', filename=img, _external=True) for img in list_product]
+    print(image_urls)
+
+    response_data = {
+        'images': image_urls,
+        'processed_image': f"data:image/png;base64,{img_base64}"  # Hình ảnh dưới dạng base64
+    }
+    return jsonify(response_data)
+    # return send_file(img_byte_arr, mimetype='image/png')
+
+@app.route('/images/<filename>')
+def get_image(filename):
+    # Trả về ảnh từ server
+    return send_file(f'image\\product_image\\{filename}', mimetype='image/png')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
